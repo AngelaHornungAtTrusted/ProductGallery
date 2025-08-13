ff22018dfad814b8cb83e9350e844f9e71ab066c
angela <angela@trustedtechsupport.com>
2025-08-12 21:28:26 -0400

    * Initial commit and beginnings of implementation

--- ProductGallery.php
+++ ProductGallery.php
@@ -1,1 +1,45 @@
+<?php
+/*
+ * Plugin Name: ProductGallery
+ * Description: Product Gallery
+ * Version: 1.0
+ * Requires at least: 5.2
+ * Requires PHP: 8.0.0
+ * Author: Angela Hornung
+ * Prefix: pg
+ */
 
+//load classes & configs
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'ProductGalleryConfig.php');
+require_once(PG_ROOT_DIR_PATH . '/vendor/autoload.php');
+require_once(PG_UTIL_DIR_PATH . '/pg-ajax.php');
+
+/* Plugin Activation & Installation Management Hooks */
+register_activation_hook(__FILE__, 'pg_activate');
+
+/* Actions */
+add_action('admin_menu', 'pg_admin_menu');
+
+function pg_activate() : void {
+
+}
+
+function pg_admin_menu() : void {
+    add_menu_page(
+        'Product Gallery Management',
+        'Product Gallery',
+        'manage_options',
+        'pg-admin',
+        'pg_admin_page_content'
+    );
+}
+
+function pg_admin_page_content() : void {
+    ?>
+    <div class="wrap">
+        <?php wp_enqueue_style('bootstrap-css', PG_ASSETS_URL . '/bootstrap/css/bootstrap.css'); ?>
+        <?php wp_enqueue_script('bootstrap-js', PG_ASSETS_URL . '/bootstrap/js/bootstrap.js'); ?>
+        <?php include(PG_ADMIN_DIR_PATH . DIRECTORY_SEPARATOR . 'admin.php'); ?>
+    </div>
+<?php
+}
---
+++ ProductGalleryConfig.php
@@ -0,0 +1,24 @@
+<?php
+define('PG_PLUGIN_NAME', 'Product Gallery');
+define('PG_PLUGIN_SLUG', 'product-gallery');
+define('PG_PLUGIN_VERSION', '1.0.0');
+
+/* Global File Paths */
+define('PG_ROOT_DIR_NAME', 'ProductGallery');
+define('PG_ROOT_DIR_PATH', plugin_dir_path(__FILE__));
+define('PG_ADMIN_DIR_PATH', PG_ROOT_DIR_PATH . 'Admin');
+define('PG_ASSETS_DIR_PATH', PG_ROOT_DIR_PATH . 'Assets');
+define('PG_SHORTCODE_DIR_PATH', PG_ROOT_DIR_PATH . 'Shortcode');
+define('PG_UTIL_DIR_PATH', PG_ROOT_DIR_PATH . 'Util');
+
+/* Global File Urls */
+define('PG_ROOT_DIR_URL', plugin_dir_url(__FILE__));
+define('PG_ADMIN_URL', PG_ROOT_DIR_URL . 'Admin');
+define('PG_ASSETS_URL', PG_ROOT_DIR_URL . 'Assets');
+define('PG_SHORTCODE_URL', PG_ROOT_DIR_URL . 'Shortcode');
+define('PG_UTIL_URL', PG_ROOT_DIR_URL . 'Util');
+
+/* Global Database Details */
+global $wpdb;
+define('PG_PLUGIN_PREFIX', 'PG');
+define('PG_DB_PREFIX', 'PG');
